3de760421fb3a08148fd95f127e636c21e0f0c35
LSH <gadlsh1208@naver.com>
2021-03-17 11:21:05 +0900

    Add Send feature with multithread

--- GameServer/ServerCore/Session.cs
+++ GameServer/ServerCore/Session.cs
@@ -9,8 +9,13 @@ namespace ServerCore
     class Session
     {
         int _disconnected = 0;
+        object _lock = new object();
 
         Socket _socket;
+        SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
+
+        Queue<byte[]> _sendQueue = new Queue<byte[]>();
+        bool _isPending = false;
 
         public void Start(Socket socket)
         {
@@ -19,12 +24,18 @@ namespace ServerCore
             recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnReceiveCompleted);
             recvArgs.SetBuffer(new byte[1024], 0, 1024);
 
+            _sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendCompleted);
+
             RegisterReceive(recvArgs);
         }
 
         public void Send(byte[] sendBuff)
         {
-            _socket.Send(sendBuff);
+            lock (_lock)
+            {
+                _sendQueue.Enqueue(sendBuff);
+                if (!_isPending) { RegisterSend(); }
+            }
         }
 
         public void Disconnect()
@@ -36,10 +47,51 @@ namespace ServerCore
         }
 
         #region Network
+
+        private void RegisterSend()
+        {
+            _isPending = true;
+
+            byte[] buff = _sendQueue.Dequeue();
+            _sendArgs.SetBuffer(buff, 0, buff.Length);
+
+            bool isPending = _socket.SendAsync(_sendArgs);
+            if (!isPending) { OnSendCompleted(null, _sendArgs); }
+        }
+
         private void RegisterReceive(SocketAsyncEventArgs args)
         {
             bool isPending = _socket.ReceiveAsync(args);
             if (!isPending) { OnReceiveCompleted(null, args); }
+        }
+
+        private void OnSendCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            lock (_lock)
+            {
+                if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+                {
+                    try
+                    {
+                        if (_sendQueue.Count > 0)
+                        {
+                            RegisterSend();
+                        }
+                        else
+                        {
+                            _isPending = false;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"OnSendCompleted Failed {e}");
+                    }
+                }
+                else
+                {
+                    Disconnect();
+                }
+            }
         }
 
         private void OnReceiveCompleted(object sender, SocketAsyncEventArgs args)
@@ -51,6 +103,7 @@ namespace ServerCore
                 {
                     string recvData = Encoding.UTF8.GetString(args.Buffer, args.Offset, args.BytesTransferred);
                     Console.WriteLine($"[From Client] {recvData}");
+
                     RegisterReceive(args);
                 }
                 catch (Exception e)
@@ -60,7 +113,7 @@ namespace ServerCore
             }
             else
             {
-                // TODO Disconnect
+                Disconnect();
             }
         }
         #endregion
